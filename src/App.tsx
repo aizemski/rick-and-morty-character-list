a7d1e3680dd52ebcb90c513c8760c2aea5733714
aizemski <aleksanderizemski@gmail.com>
2022-07-16 11:21:10 +0200

    Project init with mui and ts

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, CssBaseline, Paper, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+    </div>
+  );
+}
+
+export default App;
